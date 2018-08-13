0916cd9e3fdf09648a7c659170aa809e116d1463
YichengZheng <yichengzheng988@gmail.com>
2018-08-13 15:19:31 -0700

    add all files

---
+++ README.md
@@ -0,0 +1,5 @@
+## iOS Image Recognition App
+This  is an iOS image classifying mobile app using Google's Pre-trainged Inception v3 model. 
+
+The image select function is accomplished by using existing UIImagePickerController object.  After image is selected, it is converted to CIImage object (Core Image). Inception v3 model is loaded via Apple's CoreML frameworks. The Core Image object previously obtained is then provided to the pre-trained model for image recognition task. 
+
--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -7,19 +7,73 @@
 //
 
 import UIKit
+import CoreML
+import Vision
+import SVProgressHUD
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+
     }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        // original unedited image selected by user
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        if let userPickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage{
+
+            imageView.image = userPickedImage
+            SVProgressHUD .show()
+            guard let ciimage = CIImage(image : userPickedImage) else{
+                fatalError("Could not convert to CIImage")
+            }
+            
+            detect(image: ciimage)
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+        SVProgressHUD.dismiss()
     }
+    
+    func detect(image: CIImage){
 
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else{
+            fatalError("Loading CoreML Model failed.")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else{
+                fatalError("Model failed to process image")
+            }
+            if let firstResult = results.first{
+                self.navigationItem.title = firstResult.identifier
 
+            }
+        }
+        
+
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do{
+
+            try handler.perform([request])
+        }
+        catch{
+            print(error)
+        }
+    }
+    @IBAction func CameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
 }
 
